b9f314f0b7bbdcc2da3502bc56ad86f248a6a499
Beto Esquivel <ppbeto94@gmail.com>
2016-08-29 14:46:33 +0100

    Adds essence... Next up... Test essence.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,52 @@
 var path = require('path');
+var react = require('react');
 var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://beto.dev:3000',
     'webpack/hot/only-dev-server',
     './src/index'
   ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+  output: { 
+    path: path.join(__dirname, 'build'), 
+    filename: 'app.min.js',
+    publicPath: '/build/'
+    //publicPath: '/static/'
   },
   plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: true
+        },
+    }),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        extensions: ['.jsx', '.js'],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+      }
+    ]
   }
 };
