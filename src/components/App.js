6c14be46abb48bd5aabb0769eb16ee694a05d51e
Beto Esquivel <ppbeto94@gmail.com>
2016-08-29 20:34:47 +0100

    Adds top appbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'essence-core';
+import AppBar from 'essence-appbar';
+import Icon from 'essence-icon';
 import { Block } from 'essence-core';
 
 import {orange, grey, blue, lightblue, lightergrey} from '../colors.js';
@@ -11,9 +13,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Text type={'h1'} classes={'e-text-center'} typography={'e-display-4'} style={ {color: orange} }>
-          {'Genetic Turtle-drawn LSystems go here'}
+      <AppBar classes={'e-text-white'} style={ {backgroundColor: orange} }>
+        <Icon name={"notification-sync"} />
+        <Text classes={'e-text-right e-text-lowercase e-display-1'}>
+          {'Genetic Turtle-drawn LSystems'}
         </Text>
+      </AppBar>
 
         <Evolve />
         <MyDivider />
