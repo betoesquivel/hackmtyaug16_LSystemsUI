10766037bdda9abc2ad9e5c4145d05834747d0ab
Beto Esquivel <ppbeto94@gmail.com>
2016-08-29 18:28:12 +0100

    IndividualList lists individuals, and the main application makes a list of 10 columns in size that is centered in the screen

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return (
-      <h1>Genetic Turtle-drawn LSystems go here.</h1>
-    );
-  }
-}
